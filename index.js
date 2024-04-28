c6d51135b5acf16f90c4b6b35f67e4a37e6cb9b2
hooriya <parih3080@gmail.com>
2024-04-29 00:20:01 +0500

    firstcommit

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+// QUESTION 64:
+function combineStringWithNumber(text, number) {
+    return text + number;
+}
+let StringWithNumber = combineStringWithNumber("Age:", 30);
+console.log(StringWithNumber);
+// QUESTION 68:
+function reminder(num1, num2) {
+    return num1 % num2;
+}
+let answer = reminder(3, 2);
+console.log(answer);
+// QUESTION 67:
+function checkTwoTrue(num3, num4) {
+    return num3 && num4;
+}
+let checkBoolean = checkTwoTrue(true, false);
+console.log(checkBoolean);
---
+++ index.ts
@@ -0,0 +1,22 @@
+// QUESTION 64:
+function combineStringWithNumber(text:string , number:number){
+   return text + number;
+}
+let StringWithNumber=combineStringWithNumber("Age:",30)
+console.log(StringWithNumber);
+
+
+// QUESTION 68:
+function reminder(num1:number,num2:number){
+    return num1 % num2;
+}
+let answer=reminder(3,2);
+console.log(answer);
+
+
+// QUESTION 67:
+function checkTwoTrue(num3:boolean,num4:boolean){
+    return num3 && num4
+}
+let checkBoolean=checkTwoTrue(true,false)
+console.log(checkBoolean)
